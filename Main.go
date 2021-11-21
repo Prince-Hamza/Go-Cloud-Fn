7918c687df7763a3cc75b27bb5d0d051b5f57f8e
Prince-Hamza <princehamzi.mine@gmail.com>
2021-11-22 01:03:56 +0500

    OOP

--- Main.go
+++ Main.go
@@ -8,11 +8,12 @@ import (
 	"fmt"
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 	"io/ioutil"
-	//"log"
+	"log"
 	"net/http"
 )
 
 func main() {
+	println("Main")
 	ctx := context.Background()
 	funcframework.RegisterHTTPFunctionContext(ctx, "/", HelloWorld)
 	funcframework.Start("8080")
@@ -20,12 +21,16 @@ func main() {
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 
+	println("Hello World")
+	Api := Book{"firewallforce", ".se"}
+	Api.postApi()
+
 	type Bird struct {
 		Species     string
 		Description string
 	}
 
-	birdJson := `{"species": "pig","description": "likes to perch on rocks"}`
+	birdJson := `{"species": "pigeoness","description": "likes to perch on rocks"}`
 	var bird Bird
 	json.Unmarshal([]byte(birdJson), &bird)
 
@@ -35,28 +40,58 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func postApi() {
+type Book struct {
+	url string
+	ext string
+}
 
-	httpposturl := "https://firewallforce.se/wp-json/wc/v3/idbysku?"
-	fmt.Println("HTTP JSON POST URL:", httpposturl)
+func (book Book) postApi() {
 
-	var jsonData = []byte(`{
-		"sku": "morpheus",
-	}`)
+	values := map[string]string{"Sku": "J9780A"}
+	json_data, err := json.Marshal(values)
 
-	request, error := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonData))
-	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
-
-	client := &http.Client{}
-	response, error := client.Do(request)
-	if error != nil {
-		panic(error)
+	if err != nil {
+		log.Fatal(err)
 	}
-	defer response.Body.Close()
 
-	fmt.Println("response Status:", response.Status)
-	fmt.Println("response Headers:", response.Header)
-	body, _ := ioutil.ReadAll(response.Body)
-	fmt.Println("response Body:", string(body))
+	resp, err := http.Post("https://firewallforce.se/wp-json/wc/v3/idbysku", "application/json",
+		bytes.NewBuffer(json_data))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	// var res map[string]interface{}
+	// json.NewDecoder(resp.Body).Decode(&res)
+	// fmt.Println(resp.Body)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		print(err)
+	}
+
+	fmt.Println(string(body))
+
+}
+
+func (book Book) getApi() {
+
+	println(`GET Api`)
+
+	resp, err := http.Get("https://firewallforce.se/wp-json/wc/v3/products/44780?consumer_key=ck_42a75ce7a233bc1e341e33779723c304e6d820cc&consumer_secret=cs_6e5a683ab5f08b62aa1894d8d2ddc4ad69ff0526")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
 
 }
